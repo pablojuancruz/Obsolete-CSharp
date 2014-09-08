934a8af9f8bdd6fcd05ae6da4c94b751564df6bd
Pablo Juan <pablojuan@foryoursystem.com>
2014-09-08 17:08:42 -0300

    Commit Aplicativo Blog

---
+++ Obsolete/Farmacia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsolete
+{
+    public class Farmacia
+    {
+        public List<string> ListaRemedios{ get; set; }
+
+        [Obsolete("Usar Método ExisteRemedioContains")]
+        public bool ExisteRemedio(string nome)
+        {
+            foreach (var remedio in ListaRemedios)
+            {
+                if (remedio == nome)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public bool ExisteRemedioContains(string nome)
+        {
+            return ListaRemedios.Contains(nome);
+        }
+
+
+        
+    }
+}
---
+++ Obsolete/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsolete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> remedios= new List<string>();
+
+            remedios.Add("Dorflex");
+            remedios.Add("Neosaldina");
+            remedios.Add("Viagra");
+            remedios.Add("Paracetamol");
+
+            var farmacia = new Farmacia{ListaRemedios = remedios};
+
+            Console.WriteLine("Digite o nome do remedio a pesquisar");
+            var nomeRemedio = Console.ReadLine();
+
+            bool temRemedio = farmacia.ExisteRemedio(nomeRemedio);
+
+            //bool temRemedio = farmacia.ExisteRemedioContains(nomeRemedio);
+
+            if (temRemedio)
+                Console.Write("Remedio foi encontrado");
+            else
+                Console.Write("Remedio não foi encontrado");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
